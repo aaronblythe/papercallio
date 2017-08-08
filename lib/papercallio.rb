4294e90eb512b781457f2e2bef2454e3642bf125
Aaron Blythe <aaron.blythe@gmail.com>
2017-08-08 08:58:49 -0500

    getting the devopsdays output to work

--- lib/papercallio.rb
+++ lib/papercallio.rb
@@ -1,5 +1,6 @@
 require 'papercallio/version.rb'
 require 'papercallio/client.rb'
+require 'papercallio/devopsdays_output.rb'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
